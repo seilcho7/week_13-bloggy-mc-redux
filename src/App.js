3ab6ca74a58921d3b395a6c3a59ce7033cb540b0
Seil Cho <seilcho7@hotmail.com>
2019-05-09 14:06:31 -0400

    adds BlogDetail component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import BlogList from './containers/BlogListContainer';
+import BlogDetail from './containers/BlogDetailContainer';
 
 function App() {
   return (
     <div className="App">
       <BlogList />
+      <BlogDetail />
     </div>
   );
 }
