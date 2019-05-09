a9c2739083d965f03325e250511e7189f5fd7f54
Seil Cho <seilcho7@hotmail.com>
2019-05-09 11:32:52 -0400

    adds initialState for localStorage

--- src/index.js
+++ src/index.js
@@ -8,14 +8,6 @@ import store from './store';
 window.store = store;
 
 
-const FREQUENCY = 2000;
-const LS_KEY = 'bloggy-mc-redux';
-
-let saveInterval = setInterval(() => {
-    const state = store.getState();
-    localStorage.setItem(LS_KEY, JSON.stringify(state));
-}, FREQUENCY);
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
--- src/store.js
+++ src/store.js
@@ -5,15 +5,35 @@ import {
 
 import posts from './reducers/posts';
 
+const FREQUENCY = 2000;
+const LS_KEY = 'bloggy-mc-redux';
+
 // Pass an object to combineReducers.
 // This object should "shaped" like your state.
 const rootReducer = combineReducers({
     posts
 });
 
+// check localStorage for any previously saved app state
+// if it's there, pass as second argument to createStore
+
+// let initialState = {};
+// const dataFromLocalStorage = JSON.parse(localStorage.getItem(LS_KEY));
+// if (dataFromLocalStorage) {
+//     initialState = dataFromLocalStorage;
+// }
+
+const initialState = JSON.parse(localStorage.getItem(LS_KEY)) || {};
+
 const store = createStore(
     rootReducer,
+    initialState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+let saveInterval = setInterval(() => {
+    const state = store.getState();
+    localStorage.setItem(LS_KEY, JSON.stringify(state));
+}, FREQUENCY);
 
 export default store;
