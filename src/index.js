0be36aedcb35a5c74d30b36b477131aea8a105c4
Seil Cho <seilcho7@hotmail.com>
2019-05-09 11:22:41 -0400

    saves state to localStorage

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
 import store from './store';
 window.store = store;
 
+
 const FREQUENCY = 2000;
 const LS_KEY = 'bloggy-mc-redux';
+
 let saveInterval = setInterval(() => {
     const state = store.getState();
     localStorage.setItem(LS_KEY, JSON.stringify(state));
